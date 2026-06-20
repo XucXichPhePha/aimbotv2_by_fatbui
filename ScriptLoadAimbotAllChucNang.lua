a125f894019d0bee21f4c307174f9bce2bf7c433
Bùi Tấn Phát <dovanthang8691@gmail.com>
2026-06-20 10:07:58 +0700

    Script để chạy ở đây

--- ScriptLoadAimbotAllChucNang.lua
+++ ScriptLoadAimbotAllChucNang.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/XucXichPhePha/aimbotv2_by_fatbui/refs/heads/main/aimbotvip"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XucXichPhePha/aimbotv2_by_fatbui/refs/heads/main/aimbotvip.lua"))()
